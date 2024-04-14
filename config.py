e4a9d36d272ed1751cbdd5301c18e7367d717707
Vivek Raj Roy <83235714+unknownforall1@users.noreply.github.com>
2024-04-14 11:37:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ API_HASH = os.environ.get("API_HASH", "8339b7684eb7f4653ed032d4828ebf89")
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "1281854169:AAFZBTE1lhi_woIiwlR3OQWCDa61GGtLf9Y") 
 
-FORCE_SUB = os.environ.get("FORCE_SUB", "botXhub") 
+FORCE_SUB = os.environ.get("FORCE_SUB", "botXhub")
 
              # Don't Remove Credit @VJ_Botz
              # Subscribe YouTube Channel For Amazing Bot @Tech_VJ
@@ -21,6 +21,9 @@ FORCE_SUB = os.environ.get("FORCE_SUB", "botXhub")
 
 DB_NAME = os.environ.get("DB_NAME", "renamebotXhub")     
 
+
+LOGCHANNEL= os.environ.get("LOGCHANNEL", "-1001398318938")
+DBCHANNEL = os.environ.get("DBCHANNEL", "")
 DB_URL = os.environ.get("DB_URL", "mongodb+srv://vivekrajroy705:NL3uH0IX2skZqO5R@cluster0.2bscxuc.mongodb.net/?retryWrites=true&w=majority")
  
 FLOOD = int(os.environ.get("FLOOD", "10"))
